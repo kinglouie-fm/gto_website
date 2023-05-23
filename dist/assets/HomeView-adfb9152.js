bcc07c7522695261822ee883beac934d636057ad
kinglouie-fm <benthillen@hotmail.com>
2023-05-23 19:29:26 +0200

    added dist

--- dist/assets/HomeView-adfb9152.js
+++
@@ -1,1 +0,0 @@
-import{_ as t}from"./_plugin-vue_export-helper-c27b6911.js";import{o as e,c as _,a as s}from"./index-2fba4b94.js";const n={},a={class:"bgimg"};function r(c,o){return e(),_("div",a)}const m=t(n,[["render",r],["__scopeId","data-v-0b861180"]]),l={__name:"HomeView",setup(c){return(o,i)=>(e(),_("main",null,[s(m)]))}};export{l as default};
