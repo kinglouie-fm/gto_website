6f6596530c7c7ba8c06654bbf6be34c67874f69f
kinglouie-fm <benthillen@hotmail.com>
2023-05-23 20:21:02 +0200

    deleted files

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  publicPath: '/gto_website/',
+  base: '/gto_website/',
   plugins: [vue()],
   resolve: {
     alias: {
