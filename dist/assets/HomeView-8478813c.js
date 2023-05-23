30458e19a2fe29c531bf24197f23e80ee238b640
kinglouie-fm <benthillen@hotmail.com>
2023-05-23 20:29:05 +0200

    deleted dist

--- dist/assets/HomeView-8478813c.js
+++
@@ -1,1 +0,0 @@
-import{_ as t}from"./_plugin-vue_export-helper-c27b6911.js";import{o as e,c as _,a as s}from"./index-5bbd34ff.js";const n={},a={class:"bgimg"};function r(c,o){return e(),_("div",a)}const m=t(n,[["render",r],["__scopeId","data-v-7c61982d"]]),l={__name:"HomeView",setup(c){return(o,d)=>(e(),_("main",null,[s(m)]))}};export{l as default};
