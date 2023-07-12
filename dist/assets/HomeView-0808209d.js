75254e7cc72d531b56bf7d55f06232a2428839d1
kinglouie-fm <benthillen@hotmail.com>
2023-07-12 15:21:35 +0200

    removed dist

--- dist/assets/HomeView-0808209d.js
+++
@@ -1,1 +0,0 @@
-import{_ as t}from"./_plugin-vue_export-helper-c27b6911.js";import{o as e,c as _,a as s}from"./index-2f31c510.js";const n={},a={class:"bgimg"};function r(c,o){return e(),_("div",a)}const m=t(n,[["render",r],["__scopeId","data-v-5141bfbd"]]),f={__name:"HomeView",setup(c){return(o,d)=>(e(),_("main",null,[s(m)]))}};export{f as default};
