66cd10c45eebc2ab745c2e1e432637be848204e5
kinglouie-fm <benthillen@hotmail.com>
2023-07-12 15:28:59 +0200

    reverted changes

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
+import { fileURLToPath, URL } from 'node:url'
+
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,22 +9,10 @@ export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': resolve(__dirname, './src')
-    }
-  },
-  build: {
-    outDir: 'dist',
-    assetsDir: '',
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, './index.html')
-      }
+      '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   }
 })
-
-
-
 
 // CHAT GPT FILE STRUCTURE
 // - src/
