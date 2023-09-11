4aeba204c1025ecdfe64cd11ba471c1712f42440
kinglouie-fm <benthillen@hotmail.com>
2023-09-11 17:41:28 +0200

    cleaned vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,12 +6,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/gto_website/',
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
+  plugins: [vue()]
 })
 
 // CHAT GPT FILE STRUCTURE
