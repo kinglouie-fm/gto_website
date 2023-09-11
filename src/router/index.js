217cbc818f1a9f5c07da1712fe9cf2a6d2951615
kinglouie-fm <benthillen@hotmail.com>
2023-09-11 17:39:10 +0200

    ready for deploy

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,9 @@
 import { createRouter, createWebHistory } from 'vue-router'
-// import HomeView from '../views/HomeView.vue'
-// import AboutView from '../views/AboutView.vue'
-// import ContactView from '../views/ContactView.vue'
-// import EventsView from '../views/EventsView.vue'
-// import GalleryView from '../views/GalleryView.vue'
+import HomeView from '../views/HomeView.vue'
+import AboutView from '../views/AboutView.vue'
+import ContactView from '../views/ContactView.vue'
+import EventsView from '../views/EventsView.vue'
+import GalleryView from '../views/GalleryView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -11,35 +11,27 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('@/views/HomeView.vue')
-      // component: HomeView
+      component: HomeView
     },
     {
       path: '/about',
       name: 'about',
-      component: () => import('@/views/AboutView.vue')
-      // component: AboutView
+      component: AboutView
     },
     {
       path: '/contact',
       name: 'contact',
-      component: () => import('@/views/ContactView.vue')
-      // component: ContactView
+      component: ContactView
     },
     {
       path: '/events',
       name: 'events',
-      component: () => import('@/views/EventsView.vue')
-      // component: EventsView
+      component: EventsView
     },
     {
       path: '/gallery',
       name: 'gallery',
-      component: () => import('@/views/GalleryView.vue')
-      // component: GalleryView
+      component: GalleryView
     }
   ]
 })
