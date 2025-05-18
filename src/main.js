48f922715c8721b871dd7be5ff3044070ce962f8
Ben Thillen <79511548+kinglouie-fm@users.noreply.github.com>
2025-05-18 11:05:03 +0200

    adjusted font awesone core styles import

--- src/main.js
+++ src/main.js
@@ -5,9 +5,14 @@ import store from './store'
 import 'bootstrap/dist/css/bootstrap.css'
 import './assets/css/style.css'
 
+// 1) import the core FA styles
+import '@fortawesome/fontawesome-svg-core/styles.css'
+// 2) disable the auto-injection
+import { config, library } from '@fortawesome/fontawesome-svg-core'
+config.autoAddCss = false
+
 import { faFacebook, faInstagram, faTiktok} from '@fortawesome/free-brands-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { library } from '@fortawesome/fontawesome-svg-core'
 
 library.add(faFacebook, faInstagram, faTiktok)
 
